834030032c5478f98881262e3060c05925efe34d
Narasimha <satyansv5@gmail.com>
2023-10-02 19:44:11 +0530

    https://github.com/Narasimha2112/programs

---
+++ loop1.java
@@ -0,0 +1,15 @@
+public class loop1 
+{
+    public static void main(String[] args) {
+        boolean learning = true;
+
+        if(learning)
+        {
+            System.out.println("Java Programming");
+        }
+        else
+        {
+            System.out.println("Playing game");
+        }
+    }
+}
