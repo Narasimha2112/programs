e33cb992ffe7ef24cfc01341d7723534df93c884
Narasimha <satyansv5@gmail.com>
2023-10-02 13:51:11 +0530

    https://github.com/Narasimha2112/programs

---
+++ GetInputFromUser.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GetInputFromUser
+{
+    public static void main(String[] args) 
+    {
+        int Phno;
+        String name;
+        
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter your name:- ");
+        name = sc.nextLine();
+        
+        System.out.println("Enter your mobile number:- ");
+        Phno = sc.nextInt();
+
+        System.out.println( name + "Your mobile number is" +Phno);
+    }
+}
